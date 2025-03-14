5fba7ccf20968df93bbe8f61f90621a05b0d6539
vibinsgit <vibinsv810@gmail.com>
2025-03-14 21:47:52 +0530

    Modified code clearly

--- Basics/Lecture-1/DataTypes.java
+++ Basics/Lecture-1/DataTypes.java
@@ -2,14 +2,26 @@
 
 class Solution {
     static int dataTypeSize(String str) {
-        if(str.equals("Character")){
-            return 1;
-        }else if(str.equals("Integer") || str.equals("Float")){
-            return 4;
-        }else if(str.equals("Long") || str.equals("Double")){
-            return 8;
-        }else{
-            return -1; 
+        switch (str.toLowerCase()) { // Convert input to lowercase for case insensitivity
+            case "character":
+                return 1;
+            case "integer":
+            case "float":
+                return 4;
+            case "long":
+            case "double":
+                return 8;
+            default:
+                return -1; // Return -1 for unknown data types
         }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(dataTypeSize("Character")); // Output: 1
+        System.out.println(dataTypeSize("Integer"));   // Output: 4
+        System.out.println(dataTypeSize("Float"));     // Output: 4
+        System.out.println(dataTypeSize("Long"));      // Output: 8
+        System.out.println(dataTypeSize("Double"));    // Output: 8
+        System.out.println(dataTypeSize("String"));    // Output: -1
     }
 }
