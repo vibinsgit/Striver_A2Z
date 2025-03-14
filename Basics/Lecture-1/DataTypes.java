28260944abe86d0f5e465e40e3b14495ca34eaf2
vibinsgit <vibinsv810@gmail.com>
2025-03-14 21:35:26 +0530

    DataType added

---
+++ Basics/Lecture-1/DataTypes.java
@@ -0,0 +1,13 @@
+class Solution {
+    static int dataTypeSize(String str) {
+        if(str.equals("Character")){
+            return 1;
+        }else if(str.equals("Integer") || str.equals("Float")){
+            return 4;
+        }else if(str.equals("Long") || str.equals("Double")){
+            return 8;
+        }else{
+            return -1; 
+        }
+    }
+}
